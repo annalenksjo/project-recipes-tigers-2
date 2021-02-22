ef03bae7cd7bef9b76af3bc724c9d4d4ad18466b
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-02-22 16:58:42 +0100

    added eventlistener and searchfield

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+
 //DOM SELECTORS
 const container = document.getElementById("recipeContainer")
+const searchField = document.getElementById("site-search")
 
 //SEARCH FIELD INPUT, hard coded
 const query = "vegetarian"
 
 //API URL
-const API_URL = `https://api.edamam.com/search?q=${query}&app_id=52d2bbc8&app_key=5cbc0ebbb54690e5c3a1cbf22c6d1c06&from=0&to=10`;
+//const API_URL = `https://api.edamam.com/search?q=${query}&app_id=52d2bbc8&app_key=5cbc0ebbb54690e5c3a1cbf22c6d1c06&from=0&to=10`;
 
-
+const fetchFunction = (searchFieldInput) => {
+  const API_URL = `https://api.edamam.com/search?q=${searchFieldInput}&app_id=52d2bbc8&app_key=5cbc0ebbb54690e5c3a1cbf22c6d1c06&from=0&to=10`
 fetch(API_URL)
 .then((response) => {
   return response.json();
@@ -25,4 +28,10 @@ fetch(API_URL)
 .catch((error) => {
   console.error("Error: ", error);
 });
-
+}
+searchField.addEventListener('keydown', (event) => {
+  if(event.code = 'Enter') {
+    let searchFieldInput = searchField
+    fetchFunction(searchFieldInput)
+  }
+})
