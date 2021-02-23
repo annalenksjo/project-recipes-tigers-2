90c6a1466c8f5d6756329e2e158c134705c08720
Anna L <annalenksjo@gmail.com>
2021-02-23 11:48:36 +0100

    more styling, nav bar ok

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ fetch(API_URL)
     container.innerHTML += `
     <div class="recipe-cards">
     <img src="${data.recipe.image}" />
-      <p>${data.recipe.label}</p>
+      <p class="label">${data.recipe.label}</p>
       <p>${data.recipe.source}</p>
       <p><a href="${data.recipe.url}">Link to recipe</a></p>
       <p>Cooking time: ${data.recipe.totalTime} minutes</p>
-      </div>`;
+      <div class="recipe-card-bottom-info">Other info</div>
+      </div>
+      `;
   });
 })
 .catch((error) => {
